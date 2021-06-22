313d570c0d70bb2c9dc0ede16bc70448203816c6
Nafiseh Ebrahimi <74317517+nafasebra@users.noreply.github.com>
2021-06-22 23:15:23 +0430

    Create ButtonsCalculate.js

---
+++ src/component/ButtonsCalculate.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './ButtonsCalculate.css';
+
+export default function App({ handler }) {
+
+    return (
+        <div className="buttons-calculate">
+            <button onClick={(e) => handler(e)}> AC </button>
+            <button onClick={(e) => handler(e)}> &laquo; </button>
+            <button onClick={(e) => handler(e)}> % </button>
+            <button onClick={(e) => handler(e)}> / </button>
+
+            <button onClick={(e) => handler(e)}> 7 </button>
+            <button onClick={(e) => handler(e)}> 8 </button>
+            <button onClick={(e) => handler(e)}> 9 </button>
+            <button onClick={(e) => handler(e)}> * </button>
+            
+            <button onClick={(e) => handler(e)}> 4 </button>
+            <button onClick={(e) => handler(e)}> 5 </button>
+            <button onClick={(e) => handler(e)}> 6 </button>
+            <button onClick={(e) => handler(e)}> - </button>
+            
+            <button onClick={(e) => handler(e)}> 1 </button>
+            <button onClick={(e) => handler(e)}> 2 </button>
+            <button onClick={(e) => handler(e)}> 3 </button>
+            <button onClick={(e) => handler(e)}> + </button>
+            
+            <button onClick={(e) => handler(e)}> &plusmn; </button>
+            <button onClick={(e) => handler(e)}> 0 </button>
+            <button onClick={(e) => handler(e)}> . </button>
+            <button onClick={(e) => handler(e)}> = </button>
+        </div>
+    );
+  
+}
